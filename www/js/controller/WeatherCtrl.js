ac2d78f869c507e991e1ba16bfcbc04b72e13d3c
clement <clement.boudon@interact-iv.com>
2014-11-28 18:42:08 +0100

    App valide

---
+++ www/js/controller/WeatherCtrl.js
@@ -0,0 +1,38 @@
+app.controller('WeatherCtrl',function($scope,$http){
+
+	$scope.panel = 0;
+
+	$scope.search = function(){
+		var url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.city + "&mode=json&units=metric&cnt=10";
+		$scope.loader = true;
+		$http.get(url).success(httpSuccess).error(httpError);
+	};
+
+	$scope.geolocate = function(){
+		navigator.geolocation.getCurrentPosition(function(position){
+			$http.get("http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&mode=json&units=metric&cnt=10").success(httpSuccess).error(httpError)
+       
+		})
+	};
+
+	httpSuccess = function(response){
+		$scope.weather = response;
+		$scope.loader = false;
+		$scope.panel=1;
+	};
+	
+	httpError = function(){
+		$scope.loader = false;
+		alert('Pas de données');
+	}
+	$scope.expand = function(e){
+		$elem = $(e.currentTarget);
+		$elem.addClass('row_active').siblings().removeClass('row_active');
+	};
+
+	/*$scope.city = "Lyon";*/
+	$scope.Math=Math;
+	/*$scope.search();*/
+
+	
+});
